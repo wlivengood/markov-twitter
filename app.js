e0f4359fb202cd3e3b21381e5d8d5436e7dbc916
Will Livengood <wlivengo@gmail.com>
2017-03-27 14:16:06 -0400

    hit twitter api recursively

--- app.js
+++ app.js
@@ -28,14 +28,18 @@ app.get('/', function(req, res, next) {
 	res.sendFile(__dirname + '/index.html');
 });
 
+
 /*
-* API for getting a fake tweet for a user. TODO: Need to write a recursive function to repeatedly
-* query the Twitter API for the next 200 tweets.
+* API for getting a fake tweet from a user. Because the Twitter API only serves tweets from
+* a timeline 200 at a time, we have to recursively call the API, passing in a the oldest
+* tweet each time to get the next oldest batch of 200 tweets. After this is done once for
+* a given username, the results are cached. TODO: Implement an LRU cache for the cache.
 */
 
 let cache = {};
 
-app.get('/getTweets/:user', function(req, res, next) {
+app.get('/getTweets/:user', (req, res, next) => {
+
 	if (cache[req.params.user]) {
 		let tweet = {};
 		let cachedTweets = cache[req.params.user];
@@ -49,57 +53,45 @@ app.get('/getTweets/:user', function(req, res, next) {
 		next();
 	}
 	else {
-		let allTweets = [];
-		let tweet = {};
-
-		const getNext = function(response) {
-			allTweets = allTweets.concat(response.data);
-			if (response.data.length < 2) return Promise.resolve({data: []});
-			let max = response.data[response.data.length - 1].id_str;
-			return client.get('statuses/user_timeline', {
-				screen_name: req.params.user,
-				count: 200,
-				include_rts: false,
-				exclude_replies: true,
-				max_id: max
-			});
-		}
-
-		client.get('statuses/user_timeline', {
+		let opts = {
 			screen_name: req.params.user,
 			count: 200,
 			include_rts: false,
 			exclude_replies: true,
-		})
-		.then(getNext)
-		.then(getNext)
-		.then(getNext)
-		.then(getNext)
-		.then(getNext)
-		.then(getNext)
-		.then(getNext)
-		.then(getNext)
-		.then(getNext)
-		.then(getNext)
-		.then(getNext)
-		.then(getNext)
-		.then(getNext)
-		.then(function(response) {
-			allTweets = allTweets.concat(response.data);
-			console.log(allTweets.length);
+		}
+
+		let allTweets = []
+
+		function getNext(max=null) {
+			return new Promise((resolve, reject) => {
+				if (max) opts.max_id = max;
+				client.get('statuses/user_timeline', opts)
+				.then(function(response) {
+					if (response.data.length > 2) {
+						allTweets = allTweets.concat(response.data);
+						getNext(response.data[response.data.length - 1].id_str)
+						.then(() => resolve());
+					}
+					else {
+						resolve();
+					}
+				})
+			});
+		}
+		getNext()
+		.then(() => {
 			cache[req.params.user] = allTweets;
+			let tweet = {};
 			tweetText = allTweets.map(tweet => tweet.text);
 			tweet.name = allTweets[0].user.name;
 			tweet.userName = allTweets[0].user.screen_name;
 			tweet.thumbNailSrc = allTweets[0].user.profile_image_url;
-			// tweet.text = markov.createChain(tweetText, 30, tweetProcessing.getStarter(tweetText));
 			tweet.text = markov.createTweet(tweetText, tweetProcessing.getStarter(tweetText));
 			res.send(tweet);
 		})
-		.catch((e) => res.send(e))
+		.catch((e) => res.send(e));
 	}
-	
-})
+});
 
 app.use((err, req, res, next) => {
     console.error(err);
